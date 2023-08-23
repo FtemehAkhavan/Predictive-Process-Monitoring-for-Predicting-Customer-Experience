823f3ca68ecace12175fb769e653b41d59c01c4c
FtemehAkhavan <109350593+FtemehAkhavan@users.noreply.github.com>
2023-08-24 00:26:52 +0330

    Update and rename classification-decision tree cluster0.py to classification-decision tree.py

--- classification-decision tree.py
+++ classification-decision tree.py
@@ -3,50 +3,27 @@
 
 # # decision tree
 
-# In[2]:
-
-
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
 
-# # cluster0
-
-# In[3]:
-
-
-df = pd.read_csv('C:\\Users\\Administrator\\thesis\\BPI Challenge 2016\\feature selection\\cluster0 feature selected.csv')
+df = pd.read_csv('dataset.csv')
 df
-
-
-# In[ ]:
 
 
 y=df.Complaint
 
 
-# In[ ]:
-
-
 df.drop(['Complaint','CustomerID'], axis=1, inplace=True)
-
-
-# In[ ]:
 
 
 X=df
 X
 
 
-# In[71]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-
-
-# In[72]:
 
 
 # Create Decision Tree classifer object
@@ -59,26 +36,14 @@ clf = clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 
 
-# In[73]:
-
-
 from sklearn.metrics import accuracy_score
 accuracy_score(y_test,y_pred )
-
-
-# In[74]:
 
 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 
-# In[75]:
-
-
 X.columns
-
-
-# In[76]:
 
 
 from sklearn.tree import export_graphviz
@@ -95,96 +60,36 @@ graph.write_png('decision tree cluster0.png')
 Image(graph.create_png())
 
 
-# In[77]:
-
-
 node_counts = clf.tree_.node_count
 node_counts
-
-
-# In[78]:
-
 
 depth = clf.tree_.max_depth
 depth
 
-
-# In[79]:
-
-
 text_representation = tree.export_text(clf)
 print(text_representation)
-
-
-# In[1]:
 
 
 from sklearn.metrics import confusion_matrix
 confusion_matrix(y_test, y_pred)
 
 
-# In[81]:
-
-
 from sklearn.metrics import classification_report
 print (classification_report(y_test, y_pred))
-
-
-# In[54]:
-
-
-#import numpy as np 
-#from sklearn.tree import _tree
-
-#def find_rules(tree, features):
- #   dt= tree.tree_
-  #  def visitor(node, depth):
-   #     indent= ' ' * depth
-    #    if dt.feature[node]!=_tree.Tree_UNDEFINED:
-     #       print('{}if<{}> <= {}:'. format(indent, features[node], round(dt.threshold[node],2)))
-      #      visitor(dt.children_left[node], depth + 1)
-       #     print('{}else:'. format(indent))
-        #    visitor(dt.children_left[node], depth + 1)
-       # else:
-        #    print('{}return {}:'. format(indent, dt.value[node]))
-   # visitor(0,1)
-
-
-# In[55]:
-
-
-#find_rules(clf, X.columns)
-
-
-# In[16]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[17]:
-
-
 import matplotlib.pyplot as plt
-
-
-# In[18]:
 
 
 from sklearn import tree
 plt.figure(figsize=(75,30))
 tree.plot_tree(clf,filled=True)
 
-
-# In[57]:
-
-
-#تابع کامپلکسیتی دو تا ورودی داره آلفا که نشون دهنده عمق هست و ایمپوریتی
 path= clf.cost_complexity_pruning_path(X_train, y_train)
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
-
-
-# In[58]:
 
 
 clfs=[]
@@ -193,9 +98,6 @@ for ccp_alpha in ccp_alphas:
     clf.fit(X_train, y_train)
     clfs.append(clf)
 print("number of nodes in the last tree:{} with ccp_alpa: {}". format(clfs[-1].tree_.node_count, ccp_alphas[-10]))
-
-
-# In[59]:
 
 
 train_scores = [clf.score(X_train, y_train) for clf in clfs]
@@ -211,9 +113,6 @@ ax.legend()
 plt.show()
 
 
-# In[60]:
-
-
 clf=DecisionTreeClassifier(random_state=0, ccp_alpha=0.0000625)
 
 # Train Decision Tree Classifer
@@ -223,13 +122,7 @@ clf = clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 
 
-# In[61]:
-
-
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-
-
-# In[62]:
 
 
 from sklearn.tree import export_graphviz
@@ -246,28 +139,16 @@ graph.write_png('decision tree cluster0.png')
 Image(graph.create_png())
 
 
-# In[63]:
-
-
 node_counts = clf.tree_.node_count
 node_counts
-
-
-# In[64]:
 
 
 depth = clf.tree_.max_depth
 depth
 
 
-# In[65]:
-
-
 text_representation = tree.export_text(clf)
 print(text_representation)
-
-
-# In[ ]:
 
 
 
